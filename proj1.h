f3b65fc8338f45e4113fa4640fac1333a3ae0dc6
jaredherne <76124180+jaredherne@users.noreply.github.com>
2020-12-16 13:21:07 -0500

    Update proj1.h

--- proj1.h
+++ proj1.h
@@ -3,17 +3,6 @@
 #include <string.h>
 
 
-
-/*  **************************************************************
-
-
-/* *************************************************
-   ****************************************************
-     
-	          for processes
-
-**************************************************
-****************************************   */
 #ifndef DONE
 
 struct process {
@@ -32,6 +21,7 @@ struct process {
 void generate_random_process(struct process *P);
 void print_main_menu();
 
+//These funnctions are in an external library named proj2017.lib
 struct process *resort(struct process *P);
 struct process *sortByDate(struct process *P);
 
